8246a2e821dace31ffc1398e4375bc44aab5694d
Marcos Della Justina Schulz <markodjs@gmail.com>
2022-10-13 22:14:44 -0300

    Reactive forms, Criação, validação de campos

--- src/app/components/moment-form/moment-form.component.ts
+++ src/app/components/moment-form/moment-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms'; //imports para controle do formulario
 
 @Component({
   selector: 'app-moment-form',
@@ -8,9 +9,34 @@ import { Component, Input, OnInit } from '@angular/core';
 export class MomentFormComponent implements OnInit {
   @Input() btnText!: string;
 
+  momentForm!: FormGroup; // declarado formGroup com o nome do formGroup declarado no template 
+  // "!" usado para informar que o objeto ira existir
+
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit(): void { // usado para iniciar as funções do angular como o formulario
+    this.momentForm = new FormGroup({
+      id: new FormControl(''), //Declarar o atributo com FormControl para ser "controlovel", e ser possivel fazer as validações.
+      title: new FormControl('', [Validators.required]), //[Validators.required] define que o campo é obrigatorio
+      description: new FormControl('', [Validators.required]),
+      image: new FormControl(''),
+    })
+  // declaração do FormGroup com o nome dado no template, e aqui no component, e dentro dele é declarado todos os atributos que o formulario tem para que seja controlado de forma dinamica pelo reactive form
   }
 
+  get title() { //criado get como objeto para capturar o atributo do form criado acima
+    return this.momentForm.get('title')!;
+  }
+
+  get description() { //criado get como objeto para capturar o atributo do form criado acima
+    return this.momentForm.get('description')!;
+  }
+
+  submit() {
+    if(this.momentForm.invalid){
+      return; //trava para que se o formulario reativo estiver invalido, não continua a submiçao
+    }
+
+    console.log("Enviou formulario")
+  }
 }
