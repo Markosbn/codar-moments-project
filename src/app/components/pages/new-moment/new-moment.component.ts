9886507c9dfdfa35febde606aa62773d1e771271
Marcos Della Justina Schulz <markodjs@gmail.com>
2022-10-18 22:38:28 -0300

    implementado aula 8, inserção do formulario

--- src/app/components/pages/new-moment/new-moment.component.ts
+++ src/app/components/pages/new-moment/new-moment.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Moment } from 'src/app/interfaces/Moments';
+import { MomentService } from 'src/app/services/moment.service';
 
 @Component({
   selector: 'app-new-moment',
@@ -8,9 +10,24 @@ import { Component, OnInit } from '@angular/core';
 export class NewMomentComponent implements OnInit {
   btnText = 'Compartilhar!';
 
-  constructor() { }
+  constructor(private momentService: MomentService) { }
 
   ngOnInit(): void {
   }
 
+  async createHandler(moment: Moment){ //metodo que ira rodar quando o filho enviar os dados, declarado como assincrono pois ira aguardar o retorno da api, então ira rodar de forma separada
+    const formData = new FormData(); //formData é usado para enviar formularios que contem imagens, ele não nescessita headears
+
+    formData.append("title", moment.title);
+    formData.append("description", moment.description);
+
+    if (moment.image){
+      formData.append("image", moment.image);
+    }
+    await this.momentService.createMoment(formData).subscribe(); // consumindo service com await para permanecer em assincrono e com subscribe para ser excutado, pois vem do observable
+    // to do
+    //exibir sg
+    //redirect
+
+  }
 }
--- src/app/services/moment.service.ts
+++ src/app/services/moment.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+import { Moment } from '../interfaces/Moments';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class MomentService {
+  private baseApiUrl = environment.baseApiUrl;
+  private apiUrl = `${this.baseApiUrl}api/moments`;
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  //criado metodo para consumir a api, service chamando a url definida atraves do environment
+  createMoment(formData: FormData): Observable<FormData> {
+    return this.http.post<FormData>(this.apiUrl, formData);
+  }
 }
