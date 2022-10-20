fec9bcad120df0ce7386dd4f2b67403e1d032198
Marcos Della Justina Schulz <marcos@brazilsistem.com.br>
2022-10-20 15:47:22 -0300

    implementado mensageria, e ajustado about

--- src/app/components/pages/new-moment/new-moment.component.ts
+++ src/app/components/pages/new-moment/new-moment.component.ts
@@ -1,33 +1,37 @@
-import { Component, OnInit } from '@angular/core';
-import { Moment } from 'src/app/interfaces/Moments';
-import { MomentService } from 'src/app/services/moment.service';
+import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
+import { Moment } from "src/app/interfaces/Moments";
+import { MessagesService } from "src/app/services/messages.service";
+import { MomentService } from "src/app/services/moment.service";
 
 @Component({
-  selector: 'app-new-moment',
-  templateUrl: './new-moment.component.html',
-  styleUrls: ['./new-moment.component.css']
+  selector: "app-new-moment",
+  templateUrl: "./new-moment.component.html",
+  styleUrls: ["./new-moment.component.css"],
 })
 export class NewMomentComponent implements OnInit {
-  btnText = 'Compartilhar!';
+  btnText = "Compartilhar!";
 
-  constructor(private momentService: MomentService) { }
+  constructor(
+    private momentService: MomentService,
+    private messageService: MessagesService,
+    private router: Router,// service do angular que pode ser usar para redirecionar
+  ) {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
-  async createHandler(moment: Moment){ //metodo que ira rodar quando o filho enviar os dados, declarado como assincrono pois ira aguardar o retorno da api, então ira rodar de forma separada
+  async createHandler(moment: Moment) {
+    //metodo que ira rodar quando o filho enviar os dados, declarado como assincrono pois ira aguardar o retorno da api, então ira rodar de forma separada
     const formData = new FormData(); //formData é usado para enviar formularios que contem imagens, ele não nescessita headears
 
     formData.append("title", moment.title);
     formData.append("description", moment.description);
 
-    if (moment.image){
+    if (moment.image) {
       formData.append("image", moment.image);
     }
     await this.momentService.createMoment(formData).subscribe(); // consumindo service com await para permanecer em assincrono e com subscribe para ser excutado, pois vem do observable
-    // to do
-    //exibir sg
-    //redirect
-
+    this.messageService.add('Momento adicionado com sucesso!'); // adicionado mensagem a mensageria
+    this.router.navigate(['/']); // navigate to router usar para redirecionar para a home
   }
 }
--- src/app/services/messages.service.ts
+++ src/app/services/messages.service.ts
@@ -4,6 +4,19 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class MessagesService {
+  message: string = ''; //messagem que sera levada ao componente de mensageria
 
   constructor() { }
+
+  add(message: string){
+    this.message = message;
+
+    setTimeout(() => {
+      this.clear();
+    }, 4000); //timeout de 4 seg definido para limpar a mensagem chamando o metodo de clear para limpar mensagem
+  }
+
+  clear() {
+    this.message = '';
+  }
 }
